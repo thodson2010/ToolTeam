d3da078d94c8b040d8f7ca72d922bbf6b5e66819
Sammortinger1 <33103749+Sammortinger1@users.noreply.github.com>
2018-04-16 17:39:41 -0400

    Add files via upload

--- tool.py
+++ tool.py
@@ -13,8 +13,7 @@ arcpy.env.workspace = "C:/Users/G5223/Documents/ArcGIS/Test.gdb"
 
 # The following line is for code testing ONLY and will be overwritten automatically upon running the sript.
 shapefileInput = "C:/Users/G5223/Documents/ArcGIS/Test.gdb/states"
-shapefileOutput = "C:/Users/G5223/Documents/ArcGIS/Output.shp"
-arcpy.MakeFeatureLayer_management('states', 'lyr')
+shapefileOutput = "C:/Users/G5223/Documents/ArcGIS/Output1.shp"
 
 # The following line MUST be uncommented before use in ArcToolbox.
 #shapefileInput = arcpy.GetParameterAsText(0)
@@ -23,12 +22,13 @@ arcpy.MakeFeatureLayer_management('states', 'lyr')
 # Below, a temporary shapefile, containing the contents of the inputted shapefile, is created in the scratch geodatabase.
 numberOfColors = 5;
 #shapefile = arcpy.env.scratchGDB + os.path.sep + "temporary2.shp"
-shapefile = "C:/Users/G5223/Documents/ArcGIS/temp1.shp" #KEEP CHANGING THIS VALUE AS NECESSARY****************************************************
+shapefile = "C:/Users/G5223/Documents/ArcGIS/temp27.shp" #KEEP CHANGING THIS VALUE AS NECESSARY****************************************************
+selectedShapefile = "C:/Users/G5223/Documents/ArcGIS/Test.gdb/select27"
 
 arcpy.CopyFeatures_management(shapefileInput, shapefile)
 
 # Below, a text field is added to the temporary shapefile, which will hold the unqiue identifying code for each color group.
-newField1 = arcpy.ValidateFieldName("ColorGroup", 'lyr')
+newField1 = arcpy.ValidateFieldName("ColorGroup", shapefile)
 arcpy.AddField_management (shapefile, newField1, "TEXT")
 
 # Below, a text field is added to the temporary shapefile, which will hold a list of the color codes of the polygons that each polygon borders.
@@ -38,9 +38,9 @@ arcpy.AddField_management (shapefile, newField2, "TEXT")
 
 # Below, a text field (xyIdent) is added to the temporary shapefile, which will hold the unqiue identifying code for each individual polygon feature in the shapefile.
 arcpy.AddGeometryAttributes_management (shapefile, "CENTROID_INSIDE", "", "", "")
-newField3 = arcpy.ValidateFieldName("xyIdent", 'lyr')
+newField3 = arcpy.ValidateFieldName("xyIdent", shapefile)
 arcpy.AddField_management (shapefile, newField3, "TEXT")
-#arcpy.CalculateField_management ('lyr', "xyIdent", "str(!INSIDE_X!) + str( !INSIDE_Y!)", "PYTHON")
+arcpy.CalculateField_management (shapefile, "xyIdent", "str(!INSIDE_X!) + str( !INSIDE_Y!)", "PYTHON")
 arcpy.DeleteField_management (shapefile, "INSIDE_X")
 arcpy.DeleteField_management (shapefile, "INSIDE_Y")
 
@@ -62,7 +62,7 @@ with arcpy.da.UpdateCursor(shapefile, ["ColorGroup"]) as cursor:
             currentColor = 1 
         cursor.updateRow(row) 
 
-
+arcpy.MakeFeatureLayer_management(shapefile, 'lyr')
 
 enum1 = 0
 
@@ -75,14 +75,29 @@ print "test1"
 while (changes > 0):
     changes = 0
     print "test2"
-    with arcpy.da.UpdateCursor(shapefile, ["xyIdent", "ColorGroup"]) as cursor:
+    with arcpy.da.UpdateCursor('lyr', ["xyIdent", "ColorGroup"]) as cursor:
         for row in cursor:
-            print "test3"
-            arcpy.SelectLayerByLocation_management('lyr', "BOUNDARY_TOUCHES", shapefile, "", "NEW_SELECTION", "NOT_INVERT") #select all features that touch the selection
-            with arcpy.da.UpdateCursor(shapefile, ["xyIdent", "ColorGroup"]) as cursor2:
+            xyIdentOfFeature = row[0]
+            
+            try:
+                arcpy.Delete_management("feature1.shp")
+            except:
+                print "nothing exists"
+            arcpy.FeatureClassToFeatureClass_conversion ('lyr', "C:/Users/G5223/Documents/ArcGIS/", 'feature1')#, xyIdent == xyIdentOfFeature, '', '') 
+            
+            print row
+            selection = arcpy.SelectLayerByLocation_management('feature1', "BOUNDARY_TOUCHES", 'lyr', '', 'NEW_SELECTION', 'NOT_INVERT') #select all features that touch the selection
+            
+            try:
+                arcpy.Delete_management(selectedShapefile)
+            except:
+                print "nothing exists"
+            arcpy.CopyFeatures_management(selection, selectedShapefile)
+            with arcpy.da.SearchCursor(selectedShapefile, ["xyIdent", "ColorGroup"]) as cursor2:
                 options = ["1", "2", "3", "4", "5", "6", "7"]
                 for row2 in cursor2:
                     print options
+                    print row2
                     if row2[1] in options:
                         print row2[1]
                         newString = str(row2[1])
